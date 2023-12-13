9ca5b8a0de7f5a0e3eabf86aeb2cfe81967adbc6
Ramiro Heredia <94948260+Gugol-Ram@users.noreply.github.com>
2023-12-12 22:44:34 -0300

    crud de parents listo y testeado

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,8 +1,11 @@
 const { Router } = require("express");
 const userRoutes = require("./userRoutes");
+const parentsRoutes = require("./parentsRoutes");
 
 const router = Router();
 
 router.use(userRoutes); // Full CRUD
+
+router.use(parentsRoutes);
 
 module.exports = router;
