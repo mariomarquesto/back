73ab18e7f5baa6977902cefcd19a7a7e07879247
Ramiro Heredia <94948260+Gugol-Ram@users.noreply.github.com>
2023-12-18 21:04:06 -0300

    simulacion de DB lista

--- src/config/db.js
+++ src/config/db.js
@@ -4,38 +4,36 @@ const fs = require("fs");
 const path = require("path");
 const { DB_USER, DB_PASSWORD, DB_HOST } = process.env;
 const sequelize = new Sequelize(
-     `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/usertest`,
-     {
-          logging: false,
-          native: false,
-     }
+  `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/usertest`,
+  {
+    logging: false,
+    native: false,
+  }
 );
 
 const basename = path.basename(__filename);
 const modelDefiners = [];
 
 fs.readdirSync(path.join(__dirname, "../models"))
-     .filter(
-          (file) =>
-               file.indexOf(".") !== 0 && file !== basename && file.slice(-3) === ".js"
-     )
-     .forEach((file) => {
-          modelDefiners.push(require(path.join(__dirname, "../models", file)));
-     });
+  .filter(
+    (file) =>
+      file.indexOf(".") !== 0 && file !== basename && file.slice(-3) === ".js"
+  )
+  .forEach((file) => {
+    modelDefiners.push(require(path.join(__dirname, "../models", file)));
+  });
 
 modelDefiners.forEach((model) => model(sequelize));
 
 let entries = Object.entries(sequelize.models);
 let capsEntries = entries.map((entry) => [
-     entry[0][0].toUpperCase() + entry[0].slice(1),
-     entry[1],
+  entry[0][0].toUpperCase() + entry[0].slice(1),
+  entry[1],
 ]);
 
 sequelize.models = Object.fromEntries(capsEntries);
 
-
 const { User, Parents, Estudiante, Grade } = sequelize.models;
-
 
 User.belongsToMany(Parents, { through: "userParent" });
 Parents.belongsToMany(User, { through: "userParent" });
@@ -46,7 +44,93 @@ Estudiante.belongsToMany(Parents, { through: "parentEstudiante" });
 //Parents.belongsToMany(estudiante, { through: "parentEstudiante", as: "estudiantes", foreignKey: "userId" })
 //estudiante.belongsToMany(Parents, { through: "parentEstudiante", as: "parents", foreignKey: "estudianteId" })
 
+async function loadDataToDatabase() {
+  const userData = fs.readFileSync(path.join(__dirname, "usersData.json"));
+  const parentsData = fs.readFileSync(
+    path.join(__dirname, "parentsData.json"),
+    "utf8"
+  );
+  const studentsData = fs.readFileSync(
+    path.join(__dirname, "studentsData.json"),
+    "utf8"
+  );
+
+  const User = sequelize.models.User;
+  const Parents = sequelize.models.Parents;
+  const Estudiante = sequelize.models.Estudiante;
+
+  const users = JSON.parse(userData);
+  const parents = JSON.parse(parentsData);
+  const students = JSON.parse(studentsData);
+
+  for (const userData of users) {
+    try {
+      const [user, created] = await User.findOrCreate({
+        where: { id: userData.id },
+        defaults: userData,
+      });
+
+      console.log(
+        created ? "Usuario creado:" : "Usuario encontrado:",
+        user.toJSON()
+      );
+
+      const parentRelated = parents.filter(
+        (parent) => parent.userId === userData.id
+      );
+
+      for (const parentData of parentRelated) {
+        try {
+          const [parent, created] = await Parents.findOrCreate({
+            where: { id: parentData.id },
+            defaults: parentData,
+          });
+
+          console.log(
+            created ? "Padre creado:" : "Padre encontrado:",
+            parent.toJSON()
+          );
+
+          await user.addParents(parent);
+          console.log("Padre asociado al usuario:", parent.id);
+
+          // Encontrar estudiantes relacionados con este padre
+          const studentsRelatedToParent = students.filter(
+            (student) => student.parentId === parent.id
+          );
+
+          for (const studentData of studentsRelatedToParent) {
+            try {
+              const [student, studentCreated] = await Estudiante.findOrCreate({
+                where: { id: studentData.id },
+                defaults: studentData,
+              });
+
+              console.log(
+                studentCreated
+                  ? "Estudiante creado:"
+                  : "Estudiante encontrado:",
+                student.toJSON()
+              );
+
+              await parent.addEstudiante(student);
+              console.log("Estudiante asociado al padre:", student.id);
+            } catch (error) {
+              console.error("Error al cargar estudiante:", error);
+            }
+          }
+        } catch (error) {
+          console.error("Error al cargar padre:", error);
+        }
+      }
+    } catch (error) {
+      console.error("Error al cargar usuario:", error);
+    }
+  }
+}
+
 module.exports = {
-     ...sequelize.models, // para poder importar los modelos así: const { Product, User } = require('./db.js');
-     conn: sequelize, // para importart la conexión { conn } = require('./db.js');
+  ...sequelize.models, // para poder importar los modelos así: const { Product, User } = require('./db.js');
+  conn: sequelize, // para importart la conexión { conn } = require('./db.js');
+  loadDataToDatabase,
 };
