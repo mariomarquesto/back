193fbdda4347bb02f79eb5a1426b95613b77a511
Ramiro Heredia <94948260+Gugol-Ram@users.noreply.github.com>
2023-12-21 14:05:03 -0300

    admin routes y ctrl

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,12 +2,13 @@ const { Router } = require("express");
 const userRoutes = require("./userRoutes");
 const parentsRoutes = require("./parentsRoutes");
 const estudianteRoutes = require("./estudianteRoutes");
-
+const adminRoutes = require("./adminRoutes");
 
 const router = Router();
 
 router.use(userRoutes); // Full CRUD
-router.use(estudianteRoutes)
+router.use(estudianteRoutes);
 router.use(parentsRoutes);
+router.use(adminRoutes);
 
 module.exports = router;
