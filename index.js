76f85009e04b1152e47bf673f0cb6f0566f935e9
Jonnathan Franz <127111906+jFranzBel@users.noreply.github.com>
2024-01-16 08:15:40 -0600

    Update index.js

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
 require("dotenv").config();
 const server = require("./src/server");
 const { conn } = require("./src/config/db.js");
-//const { loadDataToDatabase } = require("./src/config/dataLoader.js");
+const { loadDataToDatabase } = require("./src/config/dataLoader.js");
 const { PORT } = process.env;
 
 (async () => {
      try {
-          await conn.sync({ force: false });
+          await conn.sync({ force: true });
           console.log("Database schema synchronized.");
 
-          //await loadDataToDatabase();
+          await loadDataToDatabase();
           server.listen(PORT, () => {
                console.log(`Server listening on port ${PORT}.`);
           });
